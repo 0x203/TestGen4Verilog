e069ab93e9bfb1ab53cad7eec86cf98982897961
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-28 01:47:10 +0200

    introduce callback hell in index.js; use tabs there instead of spaces

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
 
 const program = require('commander');
 const generateVerilogTest = require('./verilog-test-gen');
 const fs = require('fs');
 
 const STANDARD_OUTFILE = 'test.v';
+const STANDARD_TEMPLATE = 'testbench_template.v.handlebars';
 
 program
-    .arguments('<testfile>')
-    .option('-o, --outfile [outfile]', 'the file the generated test will be written into, defaults to ' + STANDARD_OUTFILE)
-    .action(function(testfile){
-        const outfile = program.outfile || STANDARD_OUTFILE;
-        console.log('should read testfile %s and write output to %s', testfile, outfile);
+	.arguments('<testfile>')
+	.option('-o, --output [file]', 'the file the generated test will be written into, defaults to ' + STANDARD_OUTFILE)
+	.option('-t, --template [file]', 'the file the template is read from, defaults to ' + STANDARD_OUTFILE)
+	.action(function(testfile){
+		const outfile = program.outfile || STANDARD_OUTFILE;
+		const templatefile = program.templatefile || STANDARD_TEMPLATE;
+		console.log('should read testfile %s and write output to %s', testfile, outfile);
 
-        fs.readFile(testfile, 'utf8', function (err, data) {
-            if (err) {
-                console.error("Can't read file with name '" + testfile + "'!");
-                process.exit(1);
-             }
-            const test = JSON.parse(data);
-            console.log('Will generate test');
-            const verilog = generateVerilogTest(test);
-            console.log('Generated test, writing it to file:');
-            fs.writeFile(outfile, verilog, function(err) {
-                if(err) {
-                    console.error("Can't write test out: " + err)
-                }
-                console.log("The file was saved! I'm done with everything.");
-            });
-        });
-    })
-    .parse(process.argv)
+		fs.readFile(testfile, 'utf8', function (err, jsontest) {
+			if (err) {
+				console.error("Can't read file with name '" + testfile + "'!");
+				process.exit(1);
+			}
+			fs.readFile(templatefile, 'utf8', function(err, template) {
+				if (err) {
+					console.error("Can't read file with name '" + testfile + "'!");
+					process.exit(1);
+				}
+				const test = JSON.parse(jsontest);
+				console.log('Will generate test');
+				generateVerilogTest(test, template, function(err, data) {
+					console.log('Generated test, writing it to file:');
+					fs.writeFile(outfile, data, function(err) {
+						if(err) {
+							console.error("Can't write test out: " + err);
+						}
+						console.log("The file was saved! I'm done with everything.");
+					});
+				});
+			});
+		});
+	})
+	.parse(process.argv);
 
