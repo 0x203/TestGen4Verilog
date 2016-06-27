553a7a342d169b104acff799d69cda41dad06c1a
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-27 21:16:43 +0200

    read and write files, call other module stub

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
 console.log('Hello, world!');
 
 const program = require('commander');
+const generateVerilogTest = require('./verilog-test-gen');
+const fs = require('fs');
 
-const STANDARD_OUTFILE = 'test.v'
+const STANDARD_OUTFILE = 'test.v';
 
 program
     .arguments('<testfile>')
     .option('-o, --outfile [outfile]', 'the file the generated test will be written into, defaults to ' + STANDARD_OUTFILE)
     .action(function(testfile){
-        const outfile = program.outfile || STANDARD_OUTFILE
-        console.log('should read testfile %s and write output to %s', testfile, outfile)
+        const outfile = program.outfile || STANDARD_OUTFILE;
+        console.log('should read testfile %s and write output to %s', testfile, outfile);
+
+        fs.readFile(testfile, 'utf8', function (err, data) {
+            if (err) {
+                console.error("Can't read file with name '" + testfile + "'!");
+                process.exit(1);
+             }
+            const test = JSON.parse(data);
+            console.log('Will generate test');
+            const verilog = generateVerilogTest(test);
+            console.log('Generated test, writing it to file:');
+            fs.writeFile(outfile, verilog, function(err) {
+                if(err) {
+                    console.error("Can't write test out: " + err)
+                }
+                console.log("The file was saved! I'm done with everything.");
+            });
+        });
     })
     .parse(process.argv)
 
