51220cc8051f3a7c586b2e9f33f72f7e19022f5b
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-27 17:46:35 +0200

    add commander and parse some cli arguments

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 console.log('Hello, world!');
 
+const program = require('commander');
+
+const STANDARD_OUTFILE = 'test.v'
+
+program
+    .arguments('<testfile>')
+    .option('-o, --outfile [outfile]', 'the file the generated test will be written into, defaults to ' + STANDARD_OUTFILE)
+    .action(function(testfile){
+        const outfile = program.outfile || STANDARD_OUTFILE
+        console.log('should read testfile %s and write output to %s', testfile, outfile)
+    })
+    .parse(process.argv)
 
