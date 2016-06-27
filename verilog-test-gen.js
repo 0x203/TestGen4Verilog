bf53f07f2df4ef5f998a00ed1719559ad07b3978
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-28 01:46:40 +0200

    WIP use handlebars to render a correct tesbench from sample data

--- verilog-test-gen.js
+++ verilog-test-gen.js
@@ -1,7 +1,19 @@
 
-function generateVerilogTest(test) {
-    return 'foo'
+const fs = require('fs');
+const handlebars = require('handlebars');
+
+function generateVerilogTest(test, template, cb) {
+	const compiledTemplate = handlebars.compile(template);
+
+	//for now, just read data from a file
+	fs.readFile('sample_tmpl_input.json', 'utf8', function(err, sampleDataJson) {
+		if(err) {
+			console.error("can't read sample input: " + err);
+		}
+		const sampleData = JSON.parse(sampleDataJson);
+		const rendered = compiledTemplate(sampleData);
+		cb(null, rendered)
+	});
 }
 
-module.exports = generateVerilogTest
-
+module.exports = generateVerilogTest;
