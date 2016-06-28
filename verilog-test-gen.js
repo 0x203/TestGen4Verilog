4a633bdb890c02e35065843a4f7e33e9d6be0334
Cornelius Bock <cornelius.bock@student.hpi.uni-potsdam.de>
2016-06-28 21:58:10 +0200

    transform remaining fileds

--- verilog-test-gen.js
+++ verilog-test-gen.js
@@ -71,19 +71,56 @@ function tranformPortsAndEvents(ports, nodes) {
 			type: (activeSense) ? 'ack' : 'req',
 			channel: channelId
 		}
-		if (activeSense && direction == 'input')
+		if (activeSense && direction == 'input') {
+			console.log("channel %d has outgoing data as active input", channelId);
 			thisOutgoing.dataWidth = dataWidth;
-		else if (!activeSense && direction == 'output')
+		}
+		else if (!activeSense && direction == 'output'){
+			console.log("channel %d has outgoing data as passive output", channelId);
 			thisOutgoing.dataWidth = dataWidth;
+		}
 		outgoing.push(thisOutgoing);
 
 		const {ingoingEvents, outgoingEvents} = splitIngoingFromOutgoing(activeSense, channelId, nodes);
 
+		// build up 'ingoing' object
+		const transformedIngoing = ingoingEvents.map(function(e) {
+			if (e.data)
+				return { eventIndex: e.id - 1, data: e.data.v };
+			else
+				return { eventIndex: e.id - 1 };
+		});
 
-		for (let {type, id, channelId, data} of ingoingEvents) {
-				type;
+		let thisIngoing = {
+			type: (activeSense) ? 'req' : 'ack',
+			channel: channelId,
+			events: transformedIngoing
 		}
+		if (!activeSense && direction == 'input'){
+			console.log("channel %d has ingoing data as passive input", channelId);
+			thisIngoing.dataWidth = dataWidth;
+		}
+		else if (activeSense && direction == 'output'){
+			console.log("channel %d has ingoing data as active output", channelId);
+			thisIngoing.dataWidth = dataWidth;
+		}
+		ingoing.push(thisIngoing);
 
+
+		// build up activeEvents
+		const newActiveEvents = outgoingEvents.map(function(e) {
+			let newActiveEvent = {
+				id: e.id - 1,
+				channel: e.channelId,
+				type: shortType(e.type)
+			};
+			if (e.data) {
+				newActiveEvent.dataWidth = dataWidth;
+				newActiveEvent.data = e.data.v;
+			}
+			return newActiveEvent;
+		});
+		activeEvents.push.apply(activeEvents, newActiveEvents);
 	}
 
 	return {uutParameters, outgoing, ingoing, activeEvents};
@@ -93,11 +130,10 @@ function splitIngoingFromOutgoing(activeSense, channelId, allEvents) {
 	const eventsOnThisPort = allEvents.filter(event => event.channelId == channelId);
 	const reqs = eventsOnThisPort.filter(event => shortType(event.type) == 'req');
 	const acks = eventsOnThisPort.filter(event => shortType(event.type) == 'ack');
-	// activeSense as seen from the inside, e.g. activate is a passive port because it's triggered from the outside
 	if(activeSense) {
-		return {ingoingEvents: acks, outgoingEvents: reqs}
-	} else {
 		return {ingoingEvents: reqs, outgoingEvents: acks}
+	} else {
+		return {ingoingEvents: acks, outgoingEvents: reqs}
 	}
 }
 
